044cf65f8aa859562ddbcec990885526bf42f499
Kaustuv Karki <karkikaustuv@gmail.com>
2023-10-21 20:21:36 +0545

    movement made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import LandingPage from './components/LandingPage/LandingPage'
-import Opening from './components/Opening/Opening'
 
 function App() {
 
   return (
     <div className='app'>
-      {/* <Opening/> */}
       <LandingPage/>
     </div>
   )
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -24,23 +24,50 @@ const LandingPage = () => {
     {showOpening ? <Opening/> :
     <div className='container'>
       <TwinklingStars/>
-      <div className="text">
+      <motion.div 
+        initial={{x: "-100vw"}}
+        animate={{x:0}}
+        transition={{ 
+        duration: 2,
+       ease: [0.16, 1, 0.3, 1]}}
+      className="text">
       <h1 className='text__h1'>Kaustuv Karki</h1>
       <p className='text__p'>SOFTWARE DEVELOPER / DESIGNER</p>
-      </div>
+      </motion.div>
     <div className='main-container'>
-        <img src={Moon} className='moon'/>
-        <img  src={Glow} className='glow'/>
+        <motion.img 
+        initial={{x: "100vw"}}
+        animate={{x:0}}
+        transition={{ 
+        duration: 1.8,
+       ease: [0.16, 1, 0.3, 1]}} 
+       src={Moon} className='moon'/>
+        <motion.img  
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{ duration: 2.5, delay: 0.4, ease: [0.16, 1, 0.3, 1]}} 
+        src={Glow} className='glow'/>
     </div>
+    <motion.div  style={{"z-index":"10"}}       
+        initial={{x: "100vw"}}
+        animate={{x:0}}
+        transition={{ 
+        duration: 3,
+        ease: [0.16, 1, 0.3, 1]}} >
     <motion.img         
       initial={{ x: -80, y: -10 }} // Initial position (move 100 pixels to the left)
       animate={{ x: 80 , y : 10}} // Final position (move 100 pixels to the right)
       transition={{ 
-        duration: 10,
+        duration: 6,
         repeat: Infinity,
         repeatType: 'mirror'
       }}
       src={Cloud} className='cloud_1'/>
+      </motion.div>
+      <motion.div className='scroll' initial={{opacity:0}} animate={{opacity:1}} transition={{duration:2, delay: 0.8}}>
+        <motion.div className="circle" initial={{y: 0, opacity:1}} animate={{y:"30px", opacity:0.2 }} transition={{duration:1.2, repeat: Infinity, repeatType: "loop"
+        }}></motion.div>
+      </motion.div>
     </div>
   }
     </>

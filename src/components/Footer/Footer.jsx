6323fe0ea0e661bd96c0ed559e25772d49c9b3a4
Kaustuv Karki <karkikaustuv@gmail.com>
2023-10-23 18:21:52 +0545

    footer complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './styles.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {AiFillGithub} from 'react-icons/ai'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
         <h1 className='email'>karkikaustuv@gmail.com</h1>
+        <div className='social-icons'>
+          <BsLinkedin/>
+          <AiFillGithub/>
+        </div>
     </div>
   )
 }

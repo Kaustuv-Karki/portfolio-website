10e6572365df9b9aa0174832e700c3e20b159ea3
Kaustuv Karki <karkikaustuv@gmail.com>
2024-02-13 20:04:14 +0545

    Overflow x problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import LandingPage from "./components/LandingPage/LandingPage";
+import MainPage from "./components/MainPage/MainPage";
 
 function App() {
   return (
     <div className="app ">
       <LandingPage />
+      <MainPage />
     </div>
   );
 }
---
+++ src/components/MainPage/MainPage.jsx
@@ -0,0 +1,7 @@
+import "./MainPage.css";
+
+const MainPage = () => {
+  return <div className="mainpage__main">MainPage</div>;
+};
+
+export default MainPage;

a87c4c19f1301d798d7d445aa403f2bd7187b182
Kaustuv <Kaustuv-Karki>
2024-02-15 14:25:18 +0545

    Downloadable CV

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import LandingPage from "./components/LandingPage/LandingPage";
+import MainLinks from "./components/MainLinks/MainLinks";
 import MainPage from "./components/MainPage/MainPage";
 
 function App() {
   return (
     <div className="app ">
+      <MainLinks/>
       <LandingPage />
       <MainPage />
+      <Footer/>
     </div>
   );
 }

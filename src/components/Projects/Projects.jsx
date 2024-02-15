149fa2229fc87031807d44f274167126aaaf4b15
Kaustuv Karki <karkikaustuv@gmail.com>
2024-02-15 22:39:23 +0545

    Projects done

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,13 +1,16 @@
-import ProjectCard from '../ProjectCard/ProjectCard';
-import './styles.css'
+import ProjectCard from "../ProjectCard/ProjectCard";
+import "./styles.css";
 const Projects = () => {
   return (
     <div className="project__main">
       <h1 className="project__title">Projects</h1>
-      <div className='project__list'>
-          <ProjectCard/>
-          <ProjectCard/>
-          <ProjectCard/>
+      <div className="project__list">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </div>
     </div>
   );
